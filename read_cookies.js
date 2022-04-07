bf28c9f7ada8d9e1c5e45a544b04ec1453e9f4cf
DaInfLoop <92693892+DaInfLoop@users.noreply.github.com>
2022-04-07 14:57:05 +0100

    Create read_cookies.js

---
+++ read_cookies.js
@@ -0,0 +1,19 @@
+if (window && window.document) {
+  window.getCookie = function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+} else {
+  console.log("Not compatible with nodejs (why would it be?)")
+}
